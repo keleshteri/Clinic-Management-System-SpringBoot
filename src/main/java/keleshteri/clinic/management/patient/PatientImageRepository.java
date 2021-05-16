2268d4d15f0e016b4d819e13ed9c729d0ec38fb5
Mohammad Mehdi Shaban Keleshteri <mehdi.shaban.keleshteri@gmail.com>
2021-05-16 22:18:00 +0800

    edit  PatientImageRepository

--- src/main/java/keleshteri/clinic/management/patient/PatientImageRepository.java
+++ src/main/java/keleshteri/clinic/management/patient/PatientImageRepository.java
@@ -1,9 +1,11 @@
 package keleshteri.clinic.management.patient;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PatientImageRepository  extends JpaRepository<PatientImage,Long> {
 
     Optional<PatientImage> findByName(String name);
