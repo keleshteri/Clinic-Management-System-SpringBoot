9f8a53feac61a1baede2785fee654138b6314d4e
Mohammad Mehdi Shaban Keleshteri <mehdi.shaban.keleshteri@gmail.com>
2021-05-20 18:37:00 +0800

    part Medicine

--- src/main/java/keleshteri/clinic/management/security/config/WebSecurityConfig.java
+++ src/main/java/keleshteri/clinic/management/security/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,22 +29,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                // Enable CORS and disable CSRF
-//                .csrf().disable()
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                // Enable CORS and disable CSRF
+                .csrf().disable()
 //                .sessionManagement()
 //                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 //                .and()
 ////                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig, secretKey))
 ////                .addFilterAfter(new JwtTokenVerifier(secretKey, jwtConfig),JwtUsernameAndPasswordAuthenticationFilter.class)
-//                .authorizeRequests()
-//                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll();
 //                .antMatchers("/api/**").hasRole(STUDENT.name())
 //                .anyRequest()
 //                .authenticated();
-//    }
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
