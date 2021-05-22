944e89714c235b928f452127b088bf5575c30ffd
Mohammad Mehdi Shaban Keleshteri <mehdi.shaban.keleshteri@gmail.com>
2021-05-22 17:55:34 +0800

    edit UnitsMeasurement

--- src/main/java/keleshteri/clinic/management/global/units_measurement/UnitsMeasurement.java
+++ src/main/java/keleshteri/clinic/management/global/units_measurement/UnitsMeasurement.java
@@ -1,7 +1,10 @@
 package keleshteri.clinic.management.global.units_measurement;
 
+import keleshteri.clinic.management.global.quantity.Quantity;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -15,6 +18,12 @@ public class UnitsMeasurement {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false,updatable = false)
     private Long id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "quantity_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Quantity quantity;
 
     @NotNull(message = "Please enter  Units Name")
     @Column(unique = true,nullable = false)
