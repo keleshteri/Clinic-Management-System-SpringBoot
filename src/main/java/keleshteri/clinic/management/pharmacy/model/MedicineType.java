1bed43a60bbb920f35ed0c6649a3a61709bfaa17
Mohammad Mehdi Shaban Keleshteri <mehdi.shaban.keleshteri@gmail.com>
2021-05-22 15:31:19 +0800

    edit  medicine types

--- src/main/java/keleshteri/clinic/management/pharmacy/model/MedicineType.java
+++ src/main/java/keleshteri/clinic/management/pharmacy/model/MedicineType.java
@@ -16,6 +16,10 @@ public class MedicineType {
     @Column(nullable = false,updatable = false)
     private Long id;
 
+    @NotNull(message = "Please Enter medicine type's Code")
+    @Column(unique = true,nullable = false)
+    private String code;
+
     @NotNull(message = "Please Enter medicine type's Name")
     @Column(unique = true,nullable = false)
     private String name;
